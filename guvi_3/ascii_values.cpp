c03ae2a7286516cffd6c0ef4ec1bb36db4a4ee62
jagandvs <dvsjmr9@gmail.com>
2017-01-28 23:25:38 +0530

    printing ascii values

---
+++ guvi_3/ascii_values.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i=0;
+    char ch;
+    for(i=0;i<256;i++)
+        {
+            cout<<" "<<ch;
+            ch=ch+1;
+        }
+}
